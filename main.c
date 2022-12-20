5db514fb1daf90907c49eec5d36617b5f43efc5c
Eden <edenroas@gmail.com>
2022-12-20 09:19:56 -0500

    not done yet

--- main.c
+++ main.c
@@ -3,11 +3,12 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <signal.h>
 #include <stdlib.h>
 #include <sys/wait.h>
 
+
 #define NUM 1000
+int pipefd[2];
 int shared_resource = 0;
 pthread_cond_t cond;
 long Endtime;
@@ -134,11 +135,55 @@ void cond1() {
     Endtime = ReturnTimeNs();
     printf("The cond time is %ld\n",(Endtime-Starttime));
 }
+void *task() {
+    // Send a signal to the task through the pipe
+    // a == lock; b == unlock
+    write(pipefd[1], "a",1);
+    // Do something
+
+    write(pipefd[1], "b",1);
+    return NULL;
+}
+
+void pipe1() {
+    //in this main we use a pipe as a simple communication mechanism between the main thread and the task.
+    // Create the pipe
+    pipe(pipefd);
+
+    Starttime = ReturnTimeNs();
+    for (int i = 0; i < NUM; i++) {
+
+        // To send a signal to the task, we use the write function to write a single character to the pipe.
+        // This will cause the read function in the task to unblock and return.
+
+        // Start the task
+        pthread_t thread;
+        pthread_create(&thread, NULL, task, NULL);
+
+        char a;
+        read(pipefd[0], &a, 1); //The task waits for a signal by reading from the pipe using the read function.
+
+        char b;
+        read(pipefd[0], &b, 1); //The task waits for a signal by reading from the pipe using the read function.
+
+        // Wait for the task to complete
+        pthread_join(thread, NULL);
+    }
+    Endtime = ReturnTimeNs();
+//    if (timeDiff < 0) timeDiff *= -1;
+    printf("The time pipe signal: %ld\n", (Endtime - Starttime));
+
+    // Close the pipe
+    close(pipefd[0]);
+    close(pipefd[1]);
+
+}
 
 
 int main() {
 
     mutex1();
+    pipe1();
     cond1();
     fcntl1();
     return 0;
